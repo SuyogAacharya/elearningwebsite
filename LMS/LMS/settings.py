6cabf4610e42fbb2a8c44392248920821983077d
entrancemaster <acharya.sulav2061@gmail.com>
2024-03-14 08:28:00 +0545

    modified

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rs_d=oe=v_8z(litt1%!pw@*t5@gs24h!2)4p3wm%fry(utmj*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -48,6 +48,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATTIC_ROOT='/static/'
+STATIC_ROOT='/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
